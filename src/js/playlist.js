a1e34eaa290d68207c2738c019defcb4f39b2224
Alex Windle <alexwindle85@gmail.com>
2016-04-19 16:46:50 -0400

    song titles to display

--- src/js/playlist.js
+++ src/js/playlist.js
@@ -12,29 +12,57 @@ export default class PlayList extends Component {
     constructor(...args){
       super(...args);
       this.state = {
-        playlist: []
+        tracks: []
       }
     }
 
+  makeListItems(track){
+  	console.log(track);
+  	return <li>{track.title}</li>
+  }
+
   componentWillMount(){
+    // allTracks = [];
+
     tracks.all().then(trackIDs => {
 
-       trackIDs.forEach(data =>{
+      let promises = trackIDs.map(data => {
+      	// trackIDs.forEach(data => {
+  
+        // console.log(data.trackID)
+        // SC.get(`/tracks/${data.trackID}`).then( trackID => {
+        //   console.log(trackID)
 
-        console.log(data.trackID)
-        SC.get(`/tracks/${data.trackID}`).then( trackID => {
-          console.log(trackID)
-          })
-        })
-      }).then(track => {
-          console.log(track)
+        //   })
+        // })
+        return SC.get(`/tracks/${data.trackID}`);
+        // SC.get(`/tracks/${data.trackID}`).then(track => {
+        // 	allTracks.push(track);
+        // });
+
+        // return Promise.all(promises);
+
+      });
+
+      return Promise.all(promises);
+    
+    }).then(tracks => {
+    	//console.log('tracks', tracks);
+    	this.setState({tracks: tracks});
+    }).catch(error => {
+    	console.log(error);
     })
-    // })
+  
   }
 
   render() {
+  	let tracks = this.state.tracks;
     return (
-      <div> Fix me </div>
+      <div> 
+	      <ul>
+	      	{ tracks.map(this.makeListItems) }
+	      </ul>
+      </div>
     )
   }
 }
