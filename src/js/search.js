3e38e58e246e54642e83b844438c5831ccc4fd04
DANIEL REVO <DANIELREVO08@GMAIL.COM>
2016-04-19 12:25:00 -0400

    Component created with correct functions

--- src/js/search.js
+++ src/js/search.js
@@ -5,9 +5,67 @@ import { Link, hashHistory } from 'react-router';
 import SC from './soundcloud';
 
 export default class Search extends Component {
+
+	constructor(...args){
+		super(...args)
+
+		this.state = {
+
+			tracks: []
+
+		}
+
+
+	}
+  
+
+	clickHandler(){
+
+		SC.get('/tracks', {q: 'kanye'}).then(tracks => {
+
+			this.setState({tracks});
+
+		});
+
+
+	}
+
+
+	getTrack(track){
+
+		return(
+
+			<li key={track.id}>
+
+				{track.title}
+
+			</li>
+
+			);
+
+	}
+
   render() {
+
+  	let { tracks } = this.state;
+
+
+
     return (
-      <div> Fix me </div>
+      <div> 
+
+      	<input type="text" placeholder="Enter a song"></input>
+      	<button onClick={::this.clickHandler} >Search</button>
+
+      		
+      	<ul className="search-results">
+      		{tracks.map(::this.getTrack)}
+
+      	</ul>
+
+      </div>
     )
   }
 }
+      		// {tracks.map(getTrack)}
+      		// <Link to=`/playlist/add/${someID}`></Link>
