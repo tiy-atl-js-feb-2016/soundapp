5d4f79992be36956c739e0ed64e353a33f48cd97
DANIEL REVO <DANIELREVO08@GMAIL.COM>
2016-04-19 17:15:35 -0400

    added material UI styles and flex-box

--- src/js/search.js
+++ src/js/search.js
@@ -2,7 +2,29 @@ import React, { Component } from 'react';
 
 import { Link, hashHistory } from 'react-router';
 
+import material from 'material-ui';
+import GridList from 'material-ui/lib/grid-list/grid-list';
+import GridTile from 'material-ui/lib/grid-list/grid-tile';
+import StarBorder from 'material-ui/lib/svg-icons/toggle/star-border';
+import IconButton from 'material-ui/lib/icon-button';
+
 import SC from './soundcloud';
+
+
+const styles = {
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+  },
+  gridList: {
+    width: 500,
+    // height: 400,
+    overflowY: 'auto',
+    marginBottom: 24,
+    marginTop: 15,
+  },
+};
 
 export default class Search extends Component {
 
@@ -38,46 +60,67 @@ export default class Search extends Component {
 
 	}
 
-
+			  
 	getTrack(track){
 		console.log('avatar', track);
 		let artwork = track.artwork_url ? track.artwork_url : './images/1355481.jpg';
+		
 		return(
 
-			<li className="search-track-item" key={track.id}>
+				<GridTile
+				  className="search-track-item"
 
-				<img src={artwork}/>
-				{track.title}
-				<Link to={`/play/${track.id}`}>Play</Link>
-				<Link to={`/playlist/add/${track.id}`}>Add to Playlist</Link> 
+				  key={track.id}
+				  title={track.title}
+				  subtitle={<span>by <b>{track.user.username}</b></span>}
+				  actionIcon={<Link to={`/playlist/add/${track.id}`}><IconButton tooltip="Add to playlist" tooltipPosition="top-left"><StarBorder color="white"/></IconButton></Link>}
+				>
 
-			</li>
-
+				<Link to={`/play/${track.id}`}><img src={artwork} title="Click to play"/></Link>
+					
+				</GridTile>
 			);
 
 	}
 
   render() {
 
-  	let { tracks } = this.state;
+	let { tracks } = this.state;
 
+	return (
+	
+		<div className="search-page"> 
 
+		<header>
+			<h1>PlayOnPlayer</h1>
+		</header>
 
-    return (
-      <div className="search-page"> 
+		  <div className="search-bar">
+			  <input type="text" placeholder="Enter a song" value={this.state.value} onChange={::this.changeHandler}></input>
+			  <button onClick={::this.clickHandler} >Search</button>
+		  </div>
 
-      	<input type="text" placeholder="Enter a song" value={this.state.value} onChange={::this.changeHandler}></input>
-      	<button onClick={::this.clickHandler} >Search</button>
+		  <div style={styles.root}>
+			<GridList
+			  cellHeight={200}
+			  style={styles.gridList}
+			>
+			  {tracks.map(::this.getTrack)}
+			</GridList>
+		  </div>
+		 </div>
 
-      		
-      	<ul className="search-results">
-      		{tracks.map(::this.getTrack)}
-
-      	</ul>
-
-      </div>
-    )
+	)
   }
 }
-      		// {tracks.map(getTrack)}
-      		// <Link to=`/playlist/add/${someID}`></Link>
+
+
+		
+				// <Link to={`/playlist/add/${track.id}`}>Add to Playlist</Link> 
+
+			
+
+			// {tracks.map(getTrack)}
+			// <Link to=`/playlist/add/${someID}`></Link>
+
+		
